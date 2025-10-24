6f505759b36e41f3d1fb34e6abac2b7cc95e7a6b
R I P Skillan <ripskillan312004@gmail.com>
2025-10-24 13:15:52 +0530

    Added a buffer reader function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::{fs::File, io::{BufRead, BufReader}};
 use clap::Parser;
 
 #[derive(Parser)]
@@ -6,8 +7,34 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
+fn check_pattern (args: Cli) {
+
+    let f = match std::fs::File::open(&args.path) {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            return;
+        }
+    };
+    let reader = std::io::BufReader::new(f);
+
+    for res in reader.lines() {
+        let line = match res{
+            Ok(line) => line,
+            Err(e) => e.to_string()
+        };
+
+        if line.contains(&args.pattern){
+            println!("{}", line);
+        }
+    };
+}
+
 fn main() {
     let args = Cli::parse();
+    check_pattern(args);
 
-    println!("pattern: {:?}, path: {:?}", args.pattern, args.path); 
+    //let content = std::fs::read_to_string(&args.path).expect("couldn't read file");
+    
+    
 }
